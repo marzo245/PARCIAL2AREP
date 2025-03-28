be228f24d9f7c3b7b2918897848786ca778e48ab
Diego <58009401+marzo245@users.noreply.github.com>
2025-03-28 09:46:33 -0500

    correccion

--- calculadorSearch/src/main/java/com/edu/escuelaing/calculadorSearch/modelo/ParametrosDTO.java
+++ calculadorSearch/src/main/java/com/edu/escuelaing/calculadorSearch/modelo/ParametrosDTO.java
@@ -1,32 +1,34 @@
 package main.java.com.edu.escuelaing.calculadorSearch.modelo;
 
-import javax.management.ListenerNotFoundException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParametrosDTO {
-    public List<Integer> lista = new ArrayList<Integer>();
-    public int value;
+    private List<Integer> lista;
+    private int value;
 
-    public void parametrosDTO(int value,List<Integer> lista){
-        this.value= value;
-        this.lista = lista;
-    }
-    public void parametroDTO(){
-
-    }
-    public void setValue(int value){
-        this.value= value;
-    }
-
-    public void setLista(List<Integer> lista){
+    public ParametrosDTO(int value, List<Integer> lista) {
+        this.value = value;
         this.lista = lista;
     }
 
-    public int getValue(){
+    public ParametrosDTO() {
+        this.lista = new ArrayList<>();
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setLista(List<Integer> lista) {
+        this.lista = lista;
+    }
+
+    public int getValue() {
         return value;
     }
 
-    public List<Integer> getLista(){
+    public List<Integer> getLista() {
         return lista;
     }
 }
