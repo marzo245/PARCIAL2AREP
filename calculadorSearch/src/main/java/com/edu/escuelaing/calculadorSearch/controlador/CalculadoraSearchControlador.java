b13d501e34aabd7aba923de47cc23a36bc5d596f
Diego <58009401+marzo245@users.noreply.github.com>
2025-03-28 09:45:53 -0500

    correccion

--- calculadorSearch/src/main/java/com/edu/escuelaing/calculadorSearch/controlador/CalculadoraSearchControlador.java
+++ calculadorSearch/src/main/java/com/edu/escuelaing/calculadorSearch/controlador/CalculadoraSearchControlador.java
@@ -1,64 +1,48 @@
 package main.java.com.edu.escuelaing.calculadorSearch.controlador;
 
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 import main.java.com.edu.escuelaing.calculadorSearch.modelo.ParametrosDTO;
 
 @RestController
 public class CalculadoraSearchControlador {
 
-
     @GetMapping("/linealSearch")
-	public int calcularLineal(@RequestParam ParametrosDTO parametrosDTO) {
+    public int calcularLineal(@RequestParam ParametrosDTO parametrosDTO) {
         int valor = parametrosDTO.getValue();
         List<Integer> lista = parametrosDTO.getLista();
         int respuesta = -1;
-        for(int i =0; i< lista.size();i++){
-            if(lista.get(i).equals(valor)){
-                respuesta = -1;
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).equals(valor)) {
+                respuesta = i; 
                 break;
             }
         }
-		return respuesta;
-	}
+        return respuesta;
+    }
 
     @GetMapping("/binarySearch")
-	public int calcularBinario(@RequestParam ParametrosDTO parametrosDTO) {
+    public int calcularBinario(@RequestParam ParametrosDTO parametrosDTO) {
         int valor = parametrosDTO.getValue();
         List<Integer> lista = parametrosDTO.getLista();
-		return calculaBinario(lista, valor);
-	}
+        return calculaBinario(lista, valor);
+    }
 
-    private int calculaBinario(List<Integer> lista,int valor){
-        int resultado = -2;
-        int medio = lista.size()/2;
+    private int calculaBinario(List<Integer> lista, int valor) {
         int inicio = 0;
-        int fin = lista.size();
-        while(resultado == -2 || inicio == fin){
-            if(inicio==fin){
-                if(valor == lista.get(medio)){
-                    resultado = medio;
-                } else{
-                    resultado = -1;
-                }
+        int fin = lista.size() - 1;
+        while (inicio <= fin) { 
+            int medio = (inicio + fin) / 2;
+            if (lista.get(medio).equals(valor)) {
+                return medio;
+            } else if (valor < lista.get(medio)) {
+                fin = medio - 1;
+            } else {
+                inicio = medio + 1;
             }
-            else if(valor<lista.get(medio)){
-                fin = medio;
-                
-                medio = medio/2;
-            }else{
-                int temp = medio;
-                medio = fin/2;
-                inicio = temp;
-            }
-            
         }
-        return resultado;
+        return -1; 
     }
 }
